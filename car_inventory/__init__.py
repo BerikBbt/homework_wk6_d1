03dbba511d012013ee39be93ef2a9dc4f3d1ea41
Berik <bbaibaturov@gmail.com>
2023-11-15 00:14:51 -0500

    homework_wk6_d2

--- car_inventory/__init__.py
+++ car_inventory/__init__.py
@@ -1,15 +1,32 @@
-from flask import Flask
-from .blueprints.site.routes import site
-
-#instatiating our Flask app
-app = Flask(__name__)
+from flask import Flask 
+from flask_migrate import Migrate 
 
 
+from .blueprints.site.routes import site 
+from .blueprints.auth.routes import auth
+from config import Config 
+from .models import login_manager, db 
 
+
+app = Flask(__name__) 
+app.config.from_object(Config)
+
+
+login_manager.init_app(app)
+login_manager.login_view = 'auth.sign_in'
+login_manager.login_message = "Hey you! Log in please!"
+login_manager.login_message_category = 'warning'
+
+#we are going to use our decorator to create our first route
 # @app.route("/")
-# def Amelias_luxury_cars():
-#     return "<p>Amelia_luxury_cars!</p>"
+# def hello_world():
+#     return "<p>Hello World!</p>"
+
 
 app.register_blueprint(site)
+app.register_blueprint(auth)
 
 
+
+db.init_app(app)
+migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -1,22 +1,32 @@
-import os 
-from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv 
 
 
-basedir = os.path.abspath(os.path.dirname(__file__)) 
 
-load_dotenv(os.path.join(basedir, '.env')) 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+
+
+load_dotenv(os.path.join(basedir, '.env'))
+
+
 
 
 class Config():
-
     """
-    Set Config variables for our flask app.
-    Using Environment variables where available otherwise
-    Create config variables.
+    
+    Create Config class which will setup out configuration variables.
+    Using Environment variables where available other create config variable.
     """
 
-    FLASK_APP = os.environ.get('FLASK_APP') 
-    FLASK_ENV = os.environ.get('FLASK_ENV')
-    FLASK_DEBUG = True 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "Thats why its a secret"  
-  
+
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASC_ENV = os.environ.get('FLASK_ENV')
+    FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
+    
+    
+    SECRET_KEY = os.environ.get('SECKET_KEY') or 'Literally whatever you want as long as its s string. Cool Beans'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
